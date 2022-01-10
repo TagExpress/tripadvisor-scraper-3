bd6bd3efe91ed553870125ab333073bcdefe74bf
bruno.mota@tag.express <brunox64@outlook.com>
2022-01-10 17:49:30 -0300

    removendo pasta .vscode

---
+++ src/main.js
@@ -0,0 +1,153 @@
+const fs = require('fs')
+const Apify = require('apify')
+const axios = require('axios')
+const selectors = require('./selectors')
+const jsdom = require('jsdom')
+const JSDOM = jsdom.JSDOM
+
+const { utils } = Apify
+const { utils: { log } } = Apify
+
+Apify.main(async () => {
+    process.env.DEV_MODE = process.env.DEV_MODE === 'true'
+
+    if (process.env.DEV_MODE) {
+        if (fs.existsSync(process.env.APIFY_LOCAL_STORAGE_DIR)) {
+            fs.rmdirSync(process.env.APIFY_LOCAL_STORAGE_DIR, {recursive: true, force: true})
+        }
+
+        await Apify.setValue('INPUT', {
+            firstReviewOnly: false,
+            url: 'https://www.tripadvisor.com.br/Hotel_Review-g303506-d536032-Reviews-Hotel_Mar_Palace_Copacabana-Rio_de_Janeiro_State_of_Rio_de_Janeiro.html'
+        })
+    }
+
+    const input = await Apify.getValue('INPUT')
+
+    log.info('INPUT', input)
+
+    globalThis.SITE_URL = new URL(input.url)
+    
+    const requestList = await Apify.openRequestList('default', [{
+        url: input.url,
+        userData: {
+            initializeHotel: true
+        }
+    }])
+
+    const requestQueue = await Apify.openRequestQueue('default')
+    const dataset = await Apify.openDataset('default')
+
+    const config = selectors.config
+    const configReviews = config.selector.config.reviews.selector
+    
+    await selectors.initialize()
+    
+    async function loadPageComplete(doc, hotel) {
+
+        const nextReviewSelector = selectors.page.nextReviewSelector
+        const nextReviewElement = doc.querySelector(nextReviewSelector)
+
+        if (!input.firstReviewOnly && nextReviewElement && !nextReviewElement.classList.contains('disabled') && nextReviewElement.getAttribute('href')) {
+            log.info('enqueuing load hotel reviews:' + nextReviewElement.getAttribute('href'))
+
+            const url = new URL(nextReviewElement.getAttribute('href'), input.url)
+
+            requestQueue.addRequest({
+                url: url.href,
+                userData: {
+                    loadReviews: true,
+                    hotel
+                }
+            })
+        } else {
+            log.info('loaded hotel:' + hotel.name)
+
+            dataset.pushData(hotel)
+        }
+    }
+
+
+    const crawler = new Apify.BasicCrawler({
+        requestList,
+        requestQueue,
+        maxRequestRetries: 1,
+        maxConcurrency: 10,
+        handleRequestTimeoutSecs: 120,
+        handleRequestFunction: async ({request}) => {
+
+            if (request.userData.initializeHotel) {
+
+                log.info('Processing Hotel: ' + request.url)
+
+                const url = request.url
+                const dom = await loadPage(url)
+                const doc = dom.window.document
+
+                const hotel = selectors.selectObject(doc, config.selector)
+
+                hotel.type = 'HOTEL'
+                hotel.webUrl = url
+                
+                const regexLocationId = /-g([0-9]+)/g
+                const regexHotelId = /-d([0-9]+)/g
+                
+                const matchLocationId = regexLocationId.exec(url)
+                const matchHotelId = regexHotelId.exec(url)
+
+                if (matchLocationId && matchLocationId[1]) {
+                    hotel.locationId = matchLocationId[1]
+                }
+
+                if (matchHotelId && matchHotelId[1]) {
+                    hotel.id = matchHotelId[1]
+                }
+
+                await loadPageComplete(doc, hotel)
+
+            } else if (request.userData.loadReviews) {
+
+                log.info('Loading Hotel Reviews: ' + request.url)
+
+                const hotel = request.userData.hotel
+                const url = request.url;
+                const dom = await loadPage(url)
+                const doc = dom.window.document
+
+                hotel.reviews = (hotel.reviews || []).concat(selectors.selectArray(doc, configReviews))
+
+                await loadPageComplete(doc, hotel)
+            }
+        },
+        handleFailedRequestFunction: async ({request, error}) => {
+            log.error('Request failed', {url: request.url, error})
+        }
+    })
+
+    log.info('start crawler')
+    await crawler.run()
+    log.info('finish crawler')
+})
+
+async function loadPage(url) {
+
+    const response = await utils.requestAsBrowser({url, languageCode: 'pt', countryCode: 'BR'})
+
+    const dom = new JSDOM(response.body, {
+        url: url,
+        referrer: url,
+        contentType: "text/html",
+        includeNodeLocations: true,
+        storageQuota: 10000000,
+        runScripts: 'dangerously'
+    })
+
+    await new Promise(resolve => {
+        dom.window.addEventListener('load', e => {
+            log.info('loaded: ' + url)
+            resolve()
+        })
+    })
+
+    return dom
+}
---
+++ src/selectors.js
@@ -0,0 +1,436 @@
+const Apify = require('apify')
+
+const { utils } = Apify
+const { utils: { log } } = Apify
+
+const monthByName = {
+    jan: '01',
+    fev: '02',
+    mar: '03',
+    abr: '04',
+    mai: '05',
+    jun: '06',
+    jul: '07',
+    ago: '08',
+    set: '09',
+    out: '10',
+    nov: '11',
+    dez: '12'
+}
+
+const config = {
+    selector: {
+        selector: 'body',
+        config: {
+            name: { 
+                selector: '#HEADING'
+            },
+            rawRanking: {
+                selector: '.KeVaw',
+                convert: value => {
+                    if (value) {
+                        const regexNumber = /([0-9\.,]+)[^0-9]+([0-9\.,]+)/g
+                        const matchNumber = regexNumber.exec(value)
+                        if (matchNumber && matchNumber[1] && matchNumber[2]) {
+                            const rankingPosition = Number(matchNumber[1].replace(',','.'))
+                            const rankingDenominator = Number(matchNumber[2].replace(',','.'))
+                            return (5 - (rankingPosition / rankingDenominator * 5)).toString()
+                        }
+                    }
+                    return null
+                }
+            },
+            rankingPosition: {
+                selector: '.rank',
+                convert: value => {
+                    if (value) {
+                        const regexNumber = /([0-9\.,]+)/g
+                        const matchNumber = regexNumber.exec(value)
+                        if (matchNumber && matchNumber[1]) {
+                            return matchNumber[1].replace(',','.')
+                        }
+                    }
+                    return null
+                }
+            },
+            rankingDenominator: {
+                selector: '.KeVaw',
+                convert: value => {
+                    if (value) {
+                        const regexNumber = /([0-9\.,]+)[^0-9]+([0-9\.,]+)/g
+                        const matchNumber = regexNumber.exec(value)
+                        if (matchNumber && matchNumber[2]) {
+                            return matchNumber[2].replace(',','.')
+                        }
+                    }
+                    return null
+                }
+            },
+            rankingString: {
+                selector: '.KeVaw'
+            },
+            rating: {
+                selector: '.bvcwU.P',
+                convert: value => (value||'').replace(',','.')
+            },
+            ratingExcellent: {
+                selector: '.ui_checkbox.dQNlC',
+                select: (doc, selector) => {
+                    const list = doc.querySelectorAll(selector)
+                    for (const group of list) {
+                        if (group.querySelector('#ReviewRatingFilter_5')) {
+                            return group.querySelector('.cpqJw')
+                        }
+                    }
+                    return null
+                }
+            },
+            ratingGood: {
+                selector: '.ui_checkbox.dQNlC',
+                select: (doc, selector) => {
+                    const list = doc.querySelectorAll(selector)
+                    for (const group of list) {
+                        if (group.querySelector('#ReviewRatingFilter_4')) {
+                            return group.querySelector('.cpqJw')
+                        }
+                    }
+                    return null
+                }
+            },
+            ratingAverage: {
+                selector: '.ui_checkbox.dQNlC',
+                select: (doc, selector) => {
+                    const list = doc.querySelectorAll(selector)
+                    for (const group of list) {
+                        if (group.querySelector('#ReviewRatingFilter_3')) {
+                            return group.querySelector('.cpqJw')
+                        }
+                    }
+                    return null
+                }
+            },
+            ratingPoor: {
+                selector: '.ui_checkbox.dQNlC',
+                select: (doc, selector) => {
+                    const list = doc.querySelectorAll(selector)
+                    for (const group of list) {
+                        if (group.querySelector('#ReviewRatingFilter_2')) {
+                            return group.querySelector('.cpqJw')
+                        }
+                    }
+                    return null
+                }
+            },
+            ratingTerrible: {
+                selector: '.ui_checkbox.dQNlC',
+                select: (doc, selector) => {
+                    const list = doc.querySelectorAll(selector)
+                    for (const group of list) {
+                        if (group.querySelector('#ReviewRatingFilter_1')) {
+                            return group.querySelector('.cpqJw')
+                        }
+                    }
+                    return null
+                }
+            },
+            hotelClass: {
+                selector: '.TkRkB.d.H0',
+                value: element => element.getAttribute('title'),
+                convert: value => {
+                    if (value) {
+                        const regexValue = /([0-9\.,]+)/g
+                        const matchValue = regexValue.exec(value)
+                        if (matchValue && matchValue[1]) {
+                            return matchValue[1].replace(',','.')
+                        }
+                    }
+                    return null
+                }
+            },
+            awards: {
+                selector: '.fZdJf.P',
+                select: (doc, selector) => doc.querySelectorAll(selector),
+                value: elements => {
+                    const currentYear = new Date().getFullYear().toString()
+                    return Array.prototype.map.call(elements, element => ({name: element.textContent, year: currentYear}))
+                }
+            },
+            numberOfReviews: {
+                selector: '.cdKMr.Mc._R.b'
+            },
+            reviewsCount: {
+                selector: '.cdKMr.Mc._R.b',
+                convert: value => value ? Number(value.replace(',','.')) : null
+            },
+            reviews: {
+                selector: {
+                    selector: '[data-test-target="HR_CC_CARD"]',
+                    config: {
+                        title: {
+                            selector: '[data-test-target="review-title"] > a'
+                        },
+                        text: {
+                            selector: '.XllAv'
+                        },
+                        rating: {
+                            selector: '[data-test-target="review-rating"] > span',
+                            value: ratingValue
+                        },
+                        stayDate: {
+                            selector: '.euPKI',
+                            convert: value => {
+                                if (value && value.includes(':')) {
+                                    const regexDate = /:\s+(.+)\s+\w+\s+(\d+)$/g
+                                    const matchDate = regexDate.exec(value)
+                                    if (matchDate && matchDate[1] && matchDate[2]) {
+                                        const mes = matchDate[1]
+                                        const ano = matchDate[2]
+                                        for (const m in monthByName) {
+                                            if (mes.startsWith(m)) {
+                                                return '01/' + monthByName[m] + '/' + ano
+                                            }
+                                        }
+                                    }
+                                }
+                                return value
+                            }
+                        },
+                        publishedDate: {
+                            selector: '.bcaHz',
+                            convert: value => {
+                                if (value) {
+                                    const monthList = Object.keys(monthByName).join('|')
+                                    
+                                    const regexDate = new RegExp(`(${monthList})\.?\\s+\\w+\\s+(\\d+)$`, 'g')
+                                    const matchDate = regexDate.exec(value)
+                                    if (matchDate && matchDate[1] && matchDate[2]) {
+                                        const mes = matchDate[1]
+                                        const ano = matchDate[2]
+                                        for (const m in monthByName) {
+                                            if (mes.startsWith(m)) {
+                                                return '01/' + monthByName[m] + '/' + ano
+                                            }
+                                        }
+                                    }
+                
+                                    const currentYear = new Date().getFullYear().toString()
+                                    const regexDate2 = new RegExp(`(\\d+)\\s+\\w+\\s+(${monthList})\.?$`, 'g')
+                                    const matchDate2 = regexDate2.exec(value)
+                                    if (matchDate2 && matchDate2[1] && matchDate2[2]) {
+                                        const dia = matchDate2[1]
+                                        const mes = matchDate2[2]
+                                        for (const m in monthByName) {
+                                            if (mes.startsWith(m)) {
+                                                return (dia+'0').substring(dia.length-1) + '/' + monthByName[m] + '/' + currentYear
+                                            }
+                                        }
+                                    }
+                
+                                    if (value.toLowerCase().endsWith('ontem')) {
+                                        const currentDate = new Date()
+                                        currentDate.setDate(currentDate.getDate()-1)
+                                        const day = '0'+currentDate.getDate()
+                                        const month = '0'+(currentDate.getMonth()+1)
+                                        const year = currentDate.getFullYear()
+                                        return day.substring(day.length-2) + '/' + month.substring(month.length-2) + '/' + year
+                                    }
+                                }
+                                return value
+                            }
+                        },
+                        userLocation: {
+                            selector: '.ShLyt'
+                        },
+                        userContributions: {
+                            selector: '.ckXjS'
+                        },
+                        userUrl: {
+                            selector: 'a.ui_social_avatar',
+                            value: element => element.getAttribute('href')
+                        },
+                        userPhotoUrl: {
+                            selector: 'a.ui_social_avatar img',
+                            value: element => element.getAttribute('src')
+                        },
+                        userDisplayName: {
+                            selector: '.ui_header_link'
+                        },
+                        absoluteUrl: {
+                            selector: '[data-test-target="review-title"] > a',
+                            value: element => {
+                                let url = element.getAttribute('href')
+                                if (!url.startsWith('http')) {
+                                    url = new URL(url, globalThis.SITE_URL).href
+                                }
+                                return url
+                            }
+                        },
+                        additionalRatings: {
+                            selector: {
+                                selector: '.fFwef.S2.H2.cUidx',
+                                config: {
+                                    rating: {
+                                        selector: '.ui_bubble_rating',
+                                        value: ratingValue,
+                                        convert: value => value ? Number(value) : null
+                                    },
+                                    ratingLabel: {
+                                        selector: 'span',
+                                        select: (doc, selector) => {
+                                            const list = doc.querySelectorAll(selector)
+                                            for (const item of list) {
+                                                if (!item.classList || item.classList.length === 0) {
+                                                    return item
+                                                }
+                                            }
+                                            return null
+                                        }
+                                    }
+                                }
+                            },
+                            select: selectArray,
+                            value: valueArray
+                        }
+                    }
+                },
+                select: selectArray,
+                value: valueArray
+            },
+            amenities: {
+                selector: '.bUmsU.f.ME.H3._c',
+                select: (doc, selector) => doc.querySelectorAll(selector),
+                value: elements => {
+                    const values = []
+                    for (const element of elements) {
+                        if (element.textContent) {
+                            values.push({descr: element.textContent})
+                        }
+                    }
+                    return values
+                }
+            },
+            ratings: {
+                selector: {
+                    selector: '.cmZRz',
+                    config: {
+                        rating: {
+                            selector: '.ui_bubble_rating',
+                            value: ratingValue
+                        },
+                        ratingLabel: {
+                            selector: '.bjRcr'
+                        }
+                    }
+                },
+                select: selectArray,
+                value: valueArray
+            }
+        }
+    },
+    select: selectObject,
+    value: valueObject
+}
+
+function selectObject(doc, selector) {
+    
+    const item = doc.querySelector(selector.selector)
+    const value = {}
+    
+    for (const field in selector.config) {
+        const config = selector.config[field]
+        const element = config.select(item, config.selector)
+
+        if (element) {
+            value[field] = config.convert(config.value(element))
+        } else {
+            log.warning('field not found in document: ' + field)
+        }
+    }
+
+    return value
+}
+
+function selectArray(doc, selector) {
+    
+    const elements = doc.querySelectorAll(selector.selector)
+    const values = []
+    
+    for (const item of elements) {
+        const value = {}
+
+        for (const field in selector.config) {
+            const config = selector.config[field]
+            const element = config.select(item, config.selector)
+
+            if (element) {
+                value[field] = config.convert(config.value(element))
+            } else {
+                log.warning('field not found in document: ' + field)
+            }
+        }
+
+        values.push(value)
+    }
+
+    return values
+}
+
+function select(doc, selector) {
+    return doc.querySelector(selector)
+}
+
+function value(element) {
+    return element.textContent
+}
+
+function valueArray(element) {
+    return element
+}
+
+function valueObject(element) {
+    return element
+}
+
+function convert(value) {
+    return value
+}
+
+function ratingValue(element) {
+    if (element.classList) {
+        const regexNumber = /bubble_([0-9]+)/g
+        for (const className of element.classList) {
+            const matchNumber = regexNumber.exec(className)
+            if (matchNumber && matchNumber[1]) {
+                return matchNumber[1]
+            }
+        }
+    }
+    
+    return null
+}
+
+async function initialize() {
+    await initilizeConfig(config.selector.config)
+}
+
+async function initilizeConfig(config) {
+    for (const field in config) {
+        config[field].select = config[field].select || select
+        config[field].value = config[field].value || value
+        config[field].convert = config[field].convert || convert
+
+        if (typeof config[field].selector !== 'string') {
+            await initilizeConfig(config[field].selector.config)
+        }
+    }
+}
+
+module.exports = {
+    page: {
+        nextReviewSelector: '.ui_pagination a.ui_button.nav.next.primary'
+    },
+    config,
+    initialize,
+    selectObject,
+    selectArray
+}
